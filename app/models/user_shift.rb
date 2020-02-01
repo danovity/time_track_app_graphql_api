e2f6da8cf3eb0b71a642a51459b432bf05154018
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-01 14:41:21 -0500

    created user_shift and user models

--- app/models/user_shift.rb
+++ app/models/user_shift.rb
@@ -1,3 +1,9 @@
 class UserShift < ApplicationRecord
+  SHIFT_ACTION_TYPES = [
+    "clock_in",
+    "clock_out"
+  ].freeze
+
   belongs_to :user
+  validates :shift_action_type, :inclusion => {:in => SHIFT_ACTION_TYPES}
 end

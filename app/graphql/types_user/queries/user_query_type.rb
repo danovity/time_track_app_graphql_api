7c9910446578c1ad9a274fa5b16557ef002e68e0
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-01 14:41:02 -0500

    added user and user_shift query types

---
+++ app/graphql/types_user/queries/user_query_type.rb
@@ -0,0 +1,14 @@
+TypesUser::Queries::UserQueryType = GraphQL::ObjectType.define do
+  field :User, TypesUser::UserType, 'Find an User by ID' do
+
+    argument :id, types.ID
+
+    resolve ->(_object, arguments, context) do
+      user_id = arguments[:id]
+
+      if user_id
+        User.find_by(id: user_id)
+      end
+    end
+  end
+end

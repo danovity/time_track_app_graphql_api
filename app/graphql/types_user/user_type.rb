b6b31b46ffc6a32cb5af08e8ed2db70dffea7dfd
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-02 17:01:57 -0500

    fixed currentShiftStatus not returning any status when its a new user

--- app/graphql/types_user/user_type.rb
+++ app/graphql/types_user/user_type.rb
@@ -8,7 +8,7 @@ TypesUser::UserType =
     field :lastName, !types.String, "User's Last Name", property: :last_name
     field :emailAddress, !types.String, "User's Email Address", property: :email_address
 
-    field :currentShiftStatus, !TypesUserShift::UserShiftType, "User's Current Shift Status" do
+    field :currentShiftStatus, TypesUserShift::UserShiftType, "User's Current Shift Status" do
       resolve ->(user, arguments, _context) do
         user_shift = UserShift.where(user_id: user.id).order("created_at").last
         user_shift

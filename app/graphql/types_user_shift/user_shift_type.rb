aee9b9f7f3d3a281837ec7028b897a716449a5c7
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-02 13:04:58 -0500

    added user field to UserShift in order to update apollo clientside automatically

--- app/graphql/types_user_shift/user_shift_type.rb
+++ app/graphql/types_user_shift/user_shift_type.rb
@@ -6,4 +6,9 @@ TypesUserShift::UserShiftType =
     field :id,        !types.ID
     field :shiftActionType, !TypesUserShift::Enums::UserShiftActionTypeEnum, "Shift Action Type", property: :shift_action_type
     field :time,      !Types::DateType, "Shift Time", property: :created_at
+    field :user, !TypesUser::UserType, "User Shift's User" do
+      resolve ->(user_shift, _arguments, _context) do
+        user_shift.user
+      end
+    end
   end

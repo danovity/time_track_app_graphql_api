7140aec69bcb0737f8c77a29b92b557be8adc4cc
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-02 10:51:27 -0500

    added field currentShiftStatus to see when the user last clocked in and out

--- app/graphql/types_user/user_type.rb
+++ app/graphql/types_user/user_type.rb
@@ -7,4 +7,14 @@ TypesUser::UserType =
     field :firstName, !types.String, "User's First Name", property: :first_name
     field :lastName, !types.String, "User's Last Name", property: :last_name
     field :emailAddress, !types.String, "User's Email Address", property: :email_address
+    field :currentShiftStatus, !TypesUserShift::UserShiftType, "User's Current Shift Status" do
+      argument :emailAddress, !types.String
+
+      resolve ->(_object, arguments, _context) do
+        email_address = arguments[:emailAddress]
+        user = User.find_by(email_address: email_address)
+        user_shift = UserShift.where(user_id: user.id).order("created_at").last
+        user_shift
+      end
+    end
   end

d3ce50a1dc65e44265f9348f5a062b96c0945327
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-02 11:39:56 -0500

    added allShiftsStatus to userType

--- app/graphql/types_user/user_type.rb
+++ app/graphql/types_user/user_type.rb
@@ -7,10 +7,17 @@ TypesUser::UserType =
     field :firstName, !types.String, "User's First Name", property: :first_name
     field :lastName, !types.String, "User's Last Name", property: :last_name
     field :emailAddress, !types.String, "User's Email Address", property: :email_address
+
     field :currentShiftStatus, !TypesUserShift::UserShiftType, "User's Current Shift Status" do
       resolve ->(user, arguments, _context) do
         user_shift = UserShift.where(user_id: user.id).order("created_at").last
         user_shift
+      end
+    end
+
+    connection :allShiftsStatus, !TypesUserShift::UserShiftType.connection_type, "A paginated list of the user's check-in check-out time" do
+      resolve ->(user, arguments, _context) do
+        UserShift.where(user_id: user.id).order("created_at")
       end
     end
   end

d13859aa2f52cb0ad4bb1a74e8b89e6c58bd46c5
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-02 15:07:52 -0500

    removed pagination for allShiftsStatus

--- app/graphql/types_user/user_type.rb
+++ app/graphql/types_user/user_type.rb
@@ -15,7 +15,7 @@ TypesUser::UserType =
       end
     end
 
-    connection :allShiftsStatus, !TypesUserShift::UserShiftType.connection_type, "A paginated list of the user's check-in check-out time" do
+    field :allShiftsStatus, !TypesUserShift::UserShiftType, "A non-paginated list of the user's check-in check-out time" do
       resolve ->(user, arguments, _context) do
         UserShift.where(user_id: user.id).order("created_at DESC")
       end

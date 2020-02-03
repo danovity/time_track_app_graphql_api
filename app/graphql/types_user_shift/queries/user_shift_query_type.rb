e40082c1c56442cc22675f0787f6a909e6456ae4
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-02 22:58:42 -0500

    changed userShift query's description

--- app/graphql/types_user_shift/queries/user_shift_query_type.rb
+++ app/graphql/types_user_shift/queries/user_shift_query_type.rb
@@ -1,5 +1,5 @@
 TypesUserShift::Queries::UserShiftQueryType = GraphQL::ObjectType.define do
-  field :userShift, TypesUserShift::UserShiftType, "A paginated list of the user's check-in check-out time" do
+  field :userShift, TypesUserShift::UserShiftType, "An user's clock-in or clock-out time" do
     argument :id, types.ID
 
     resolve ->(_object, arguments, _context) do

ab643853f684be5c51081a3ec38b581e2579c677
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-17 11:27:47 -0500

    added new routes that would check deployment status

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/check.txt', to: proc {[200, {}, ['it_works']]}
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

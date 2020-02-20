b6ec2e3a63db0f3078d95ff3e6dd4c42722836fc
Sean <sean.xiaoxu.ye@gmail.com>
2020-02-20 05:01:05 -0500

    corrected argument to arguments

--- app/graphql/types_user_shift/mutations/user_shift_delete.rb
+++ app/graphql/types_user_shift/mutations/user_shift_delete.rb
@@ -7,7 +7,7 @@ TypesUserShift::Mutations::UserShiftDelete = GraphQL::ObjectType.define do
     argument :user_id, !types.ID
 
     resolve ->(_object, arguments, _context) do
-      user_shift = UserShift.find_by(id: argument[:user_shift_id], user_id: argument[:user_id])
+      user_shift = UserShift.find_by(id: arguments[:user_shift_id], user_id: arguments[:user_id])
 
       if user_shift.deactivated!
         user_shift
